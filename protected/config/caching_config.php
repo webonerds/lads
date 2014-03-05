1624562e32f1d4590c0035df3d25cd57a83e3b59
vilaswebonerds <vilas@webonerds.in>
2014-03-05 17:30:18 +0530

    protected removed

--- protected/config/caching_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-            'class' => 'CMemCache',
-            'servers' => array(
-                array('host' => '127.0.0.1', 'port' => 11211, 'weight' => 60),
-            ),
-        );
-		
